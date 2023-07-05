e135897c88c47a885d765be30a1905f78f140514
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-07-05 11:18:09 +0300

    use route parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'This method return all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action return id -${id} of the coffees`;
   }
 }
