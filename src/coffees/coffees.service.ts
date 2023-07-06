2ca438893a94c0e67b219d179f3d586221a0ce33
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-07-06 15:44:43 +0300

    Retrieve entities with their relations

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -18,12 +18,19 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: {
+        flavors: true,
+      },
+    });
   }
 
   async findOne(id: string) {
     const coffee = await this.coffeeRepository.findOne({
       where: { id: +id },
+      relations: {
+        flavors: true,
+      },
     });
 
     if (!coffee) {
