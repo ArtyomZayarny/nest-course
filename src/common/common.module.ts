a7a64560733faa0b30327a5adcac09fec3830ea3
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-07-11 13:26:43 +0300

    add request logging middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestMiddleware,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
@@ -12,4 +18,14 @@ import { ConfigModule } from '@nestjs/config';
     },
   ],
 })
-export class CommonModule {}
+export class CommonModule implements NestMiddleware {
+  use(req: any, res: any, next: (error?: any) => void) {
+    throw new Error('Method not implemented!!');
+  }
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes({
+      path: 'coffees',
+      method: RequestMethod.GET,
+    });
+  }
+}
